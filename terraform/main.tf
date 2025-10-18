585902a6b403d1f39c07286ea90a4fa270f88aff
res <211143915+urbanconfig@users.noreply.github.com>
2025-10-18 12:46:25 +0000

    variable name fix for main config file

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,5 +14,5 @@ provider "aws" {
 module "backend" {
   source        = "./modules/remote_backend"
   iam_user_name = var.iam_user_name
-  bucket_name   = var.tfstate_bucket_name
+  tfstate_bucket_name = var.tfstate_bucket_name
 }
