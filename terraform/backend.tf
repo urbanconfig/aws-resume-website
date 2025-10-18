9a3432d3c82529d7b78d7b9479a065a1f8f35ff2
res <211143915+urbanconfig@users.noreply.github.com>
2025-10-18 14:44:26 +0000

    Apply terraform fmt changes

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket              = "tfstate-urbanconfig-cloud-resume"
-    key                 = "terraform.tfstate"
-    region              = "eu-central-1"
-    use_lockfile        = true
+    bucket       = "tfstate-urbanconfig-cloud-resume"
+    key          = "terraform.tfstate"
+    region       = "eu-central-1"
+    use_lockfile = true
   }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 module "backend" {
-  source        = "./modules/remote_backend"
-  iam_user_name = var.iam_user_name
+  source              = "./modules/remote_backend"
+  iam_user_name       = var.iam_user_name
   tfstate_bucket_name = var.tfstate_bucket_name
 }
