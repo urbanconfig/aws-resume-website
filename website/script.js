22c114e53378d4b03d57a074822d8fba414c3ee9
res <211143915+urbanconfig@users.noreply.github.com>
2025-10-21 21:18:10 +0000

    Modified javascript fetch url to new lambda function

--- website/script.js
+++ website/script.js
@@ -1,6 +1,6 @@
 const counter = document.getElementById("counter-number");
 async function updateCounter() {
-    let response = await fetch("https://gaqytg2r1f.execute-api.eu-central-1.amazonaws.com");
+    let response = await fetch("https://2o2bb5tvcmlgmha2tei3jf7hky0hytob.lambda-url.eu-central-1.on.aws/");
     let data = await response.json();
     console.log(data);
     counter.innerText = data;
